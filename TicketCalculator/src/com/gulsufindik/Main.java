334a1380638db6c99f36e125879997f6739b44dc
gulsufindik <findik.gulsu@gmail.com>
2023-03-18 23:09:42 +0300

    ChineseZodiacCalculator

--- TicketCalculator/src/com/gulsufindik/Main.java
+++ TicketCalculator/src/com/gulsufindik/Main.java
@@ -3,49 +3,46 @@ package com.gulsufindik;
 import java.util.Scanner;
 
 public class Main {
-		
-		public static void main(String[] args) {
-			
-	        Scanner input = new Scanner(System.in);
 
-	        int distance, age, travelType;
-	        double ticketPrice, discountRate = 0, totalPrice;
+	public static void main(String[] args) {
 
-	        System.out.print("Enter the distance in kilometers: ");
-	        distance = input.nextInt();
+		Scanner input = new Scanner(System.in);
 
-	        System.out.print("Enter your age: ");
-	        age = input.nextInt();
+		int distance, age, travelType;
+		double ticketPrice, discountRate = 0, totalPrice;
 
-	        System.out.print("Enter trip type (1 => One Way, 2 => Round Trip): ");
-	        travelType = input.nextInt();
+		System.out.print("Enter the distance in kilometers: ");
+		distance = input.nextInt();
 
-	        if (distance > 0 && age > 0 && (travelType == 1 || travelType == 2)) {
-	            ticketPrice = distance * 0.10;
+		System.out.print("Enter your age: ");
+		age = input.nextInt();
 
-	            if (age < 12) {
-	                discountRate = 0.50;
-	            } else if (age >= 12 && age <= 24) {
-	                discountRate = 0.10;
-	            } else if (age >= 65) {
-	                discountRate = 0.30;
-	            }
+		System.out.print("Enter trip type (1 => One Way, 2 => Round Trip): ");
+		travelType = input.nextInt();
 
-	            if (travelType == 2) {
-	                discountRate += 0.20;
-	            }
+		if (distance > 0 && age > 0 && (travelType == 1 || travelType == 2)) {
+			ticketPrice = distance * 0.10;
 
-	            totalPrice = ticketPrice - (ticketPrice * discountRate);
-	            System.out.println("Total Ticket Price: " + totalPrice + " TL");
+			if (age < 12) {
+				discountRate = 0.50;
+			} else if (age >= 12 && age <= 24) {
+				discountRate = 0.10;
+			} else if (age >= 65) {
+				discountRate = 0.30;
+			}
 
-	        } else {
-	            System.out.println("You Entered Wrong Data!");
-	        }
+			if (travelType == 2) {
+				discountRate += 0.20;
+			}
 
-	        input.close();
-	    }
-	
+			totalPrice = ticketPrice - (ticketPrice * discountRate);
+			System.out.println("Total Ticket Price: " + totalPrice + " TL");
 
+		} else {
+			System.out.println("You Entered Wrong Data!");
+		}
+
+		input.close();
 	}
 
-
+}
