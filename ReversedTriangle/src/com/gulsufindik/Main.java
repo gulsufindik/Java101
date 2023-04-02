c791430b865250feab3051404a4386a2fae08550
gulsufindik <findik.gulsu@gmail.com>
2023-04-03 00:06:44 +0300

    for

---
+++ ReversedTriangle/src/com/gulsufindik/Main.java
@@ -0,0 +1,19 @@
+package com.gulsufindik;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int i, j, k;
+		for (i = 10; 0 < i; i--) {
+			for (j = i; j < 10; j++) {
+				System.out.print(" ");
+			}
+			for (k = 1; k <= i; k++) {
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
