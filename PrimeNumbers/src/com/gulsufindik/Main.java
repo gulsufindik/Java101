6f6b54d34c65e04de9464d295895552f3f570427
gulsufindik <findik.gulsu@gmail.com>
2023-04-03 00:19:31 +0300

    for-if

---
+++ PrimeNumbers/src/com/gulsufindik/Main.java
@@ -0,0 +1,23 @@
+package com.gulsufindik;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, j;
+        boolean isPrime;
+        
+        for (i = 2; i <= 100; i++) {
+            isPrime = true;
+            
+            for (j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
