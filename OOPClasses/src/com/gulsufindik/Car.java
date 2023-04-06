8637cf333bcc0c5f35b0566c8cd3b7d14642eb9d
gulsufindik <findik.gulsu@gmail.com>
2023-04-06 23:08:47 +0300

    OOP

--- OOPClasses/src/com/gulsufindik/Car.java
+++ OOPClasses/src/com/gulsufindik/Car.java
@@ -2,4 +2,28 @@ package com.gulsufindik;
 
 public class Car {
 
+	// <variables>
+	String type;
+	String model;
+	String color;
+	int speed;
+	int speedLimit = 100;
+
+	// <methods>
+	void increaseSpeed(int increment) {
+		if ((speed + increment) < speedLimit) {
+			speed += increment;
+		}
+	}
+
+	void decreaseSpeed(int decrease) {
+		if (speed > 0) {
+			speed -= decrease;
+		}
+	}
+
+	void printSpeed() {
+		System.out.println("Hızınız : " + speed);
+	}
+
 }
