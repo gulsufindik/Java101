503584eed46396b2e0aa2506aa2dd44cd4ab52a0
gulsufindik <findik.gulsu@gmail.com>
2023-04-12 13:29:38 +0300

    enum

--- HW/src/com/gulsufindik/Manager.java
+++ HW/src/com/gulsufindik/Manager.java
@@ -1,25 +1,26 @@
 package com.gulsufindik;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Manager {
-	
-	private Random random = new Random();
+    private Random random = new Random();
+    private Scanner scanner = new Scanner(System.in);
 
     public void Menu() {
-        for (EGun gun : EGun.values()) {
-            ECorba corba = ECorba.values()[random.nextInt(ECorba.values().length)];
-            EIcecek icecek = EIcecek.values()[random.nextInt(EIcecek.values().length)];
-            EYemek yemek = EYemek.values()[random.nextInt(EYemek.values().length)];
-            ETatli tatli = ETatli.values()[random.nextInt(ETatli.values().length)];
+        System.out.println("Hangi günün menüsünü görmek istersiniz?");
+        String input = scanner.nextLine().toUpperCase();
+        EGun gun = EGun.valueOf(input);
 
-            System.out.println(gun + " Menüsü:");
-            System.out.println("Çorba: " + corba);
-            System.out.println("İçecek: " + icecek);
-            System.out.println("Yemek: " + yemek);
-            System.out.println("Tatlı: " + tatli);
-            System.out.println();
-        }
+        ECorba corba = ECorba.values()[random.nextInt(ECorba.values().length)];
+        EIcecek icecek = EIcecek.values()[random.nextInt(EIcecek.values().length)];
+        EYemek yemek = EYemek.values()[random.nextInt(EYemek.values().length)];
+        ETatli tatli = ETatli.values()[random.nextInt(ETatli.values().length)];
+
+        System.out.println(gun + " Menüsü:");
+        System.out.println("Çorba: " + corba);
+        System.out.println("İçecek: " + icecek);
+        System.out.println("Yemek: " + yemek);
+        System.out.println("Tatlı: " + tatli);
+        System.out.println();
     }
 }
-
-
